9c59343a74c6596a6552ea7d40523a71b722badd
prim <artstudioexp@gmail.com>
2022-04-02 20:48:00 +0300

    Add struct Colors with color constants. Add various convinience functions to the Tools class

--- tools.cpp
+++ tools.cpp
@@ -2,6 +2,8 @@
 #include "prim_exception.hpp"
 #include <QApplication>
 #include <QMouseEvent>
+#include <nameof.hpp>
+#include <sstream>
 
 std::string Tools::floatToString(float f, uint8_t precision)
 {
@@ -25,4 +27,69 @@ void Tools::init(MainWindow* mainWindow)
 MainWindow* Tools::getMainWindow()
 {
     return mw;
+}
+
+Distance Tools::getRelevantDistance(float value, DistanceUnit currentDistanceUnit)
+{
+    static auto checkBounds = [](DistanceUnit unit) -> bool {
+        return ((int)unit + 1 < enum_size<DistanceUnit>::value && (int)unit > 0);
+    };
+
+    Distance result(value, currentDistanceUnit);
+
+    if(!checkBounds(currentDistanceUnit) || value == 0.0f) return result;
+
+    if(result.first > 1000.0f)
+    {
+        do
+        {
+            result.first *= 0.001;
+            result.second = (DistanceUnit)((int)result.second + 1);
+        }
+        while(result.first > 1000.0f && checkBounds(result.second));
+        return result;
+    }
+    else if(result.first < 1.0f)
+    {
+        do
+        {
+            result.first *= 1000.0f;
+            result.second = (DistanceUnit)((int)result.second - 1);
+        }
+        while(result.first < 1.0f && checkBounds(result.second));
+        return result;
+    }
+    else
+    {
+        return result;
+    }
+}
+
+Distance Tools::getRelevantDistance(Distance distance)
+{
+    return getRelevantDistance(distance.first, distance.second);
+}
+
+std::vector<std::string> Tools::splitString(const std::string &str, char separator)
+{
+    std::stringstream ss(str);
+    std::string segment;
+    std::vector<std::string> result;
+
+    while(std::getline(ss, segment, separator))
+    {
+        result.push_back(segment);
+    }
+
+    return std::move(result);
+}
+
+DistanceUnit Tools::stringToDistanceUnit(const std::string& str)
+{
+    for(int i = 0; i < enum_size<DistanceUnit>::value; ++i)
+    {
+        if(str == NAMEOF((DistanceUnit)i))
+            return (DistanceUnit)i;
+    }
+    return DistanceUnit::nm;
 }
--- tools.hpp
+++ tools.hpp
@@ -4,7 +4,12 @@
 #include <string>
 #include <iomanip>
 #include <QColor>
+#include <utility>
 #include "mainwindow.h"
+
+enum class DistanceUnit { pm, nm, mkm, mm, m, km };
+typedef std::pair<float, DistanceUnit> Distance;
+
 
 class Tools
 {
@@ -12,10 +17,39 @@ private:
     inline static MainWindow* mw= nullptr;
     inline static bool initialized = false;
 public:
+
+
     static void init(MainWindow* mainWindow);
     static std::string floatToString(float f, uint8_t precision);
     static void print(std::string line, QColor colorj = QColorConstants::White);
     static MainWindow* getMainWindow();
+    static Distance getRelevantDistance(float value, DistanceUnit currentDistanceUnit = DistanceUnit::nm);
+    static Distance getRelevantDistance(Distance distance);
+    static std::vector<std::string> splitString(const std::string& str, char separator);
+    static DistanceUnit stringToDistanceUnit(const std::string& str);
+};
+
+
+template<class T> struct enum_size;
+template<> struct enum_size<DistanceUnit> { static const size_t value = 6; };
+
+struct Colors
+{
+    inline static const QColor red = QColor(255, 0, 0);
+    inline static const QColor blue = QColor(0, 0, 255);
+    inline static const QColor lime = QColor(0, 255, 0);
+    inline static const QColor white = QColor(255, 255, 255);
+    inline static const QColor black = QColor(0, 0, 0);
+    inline static const QColor cyan = QColor(0,255,255);
+    inline static const QColor magenta = QColor(255,0,255);
+    inline static const QColor silver = QColor(192,192,192);
+    inline static const QColor gray = QColor(128,128,128);
+    inline static const QColor maroon = QColor(128,0,0);
+    inline static const QColor olive = QColor(128,128,0);
+    inline static const QColor purple = QColor(128,0,128);
+    inline static const QColor green = QColor(0,128,0);
+    inline static const QColor teal = QColor(0,128,128);
+    inline static const QColor navy = QColor(0,0,128);
 };
 
 #endif // TOOLS_HPP
